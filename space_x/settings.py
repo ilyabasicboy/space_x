978d42e1bf18529437df21746a35618cc5e84578
ilya.basyrov <ilya.basyrov@redsolution.ru>
2023-07-10 03:31:49 +0500

    change models

--- space_x/settings.py
+++ space_x/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9sdes2&(zpitp+qsmf7oa4)=5sta0)&bif!1+c)8_j5)d*_va5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
